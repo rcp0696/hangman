1b7283bbb9d7f485b06bd3bee42960ed68ac7f6a
Rishi <rcp0696@gmail.com>
2023-10-22 20:37:06 +0100

    Adding more words to the dictionary

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import random
 from math import *
 
 # Creating dictionary of words
-words = {1: 'India', 2: 'Britain', 3: 'Python', 4: 'Ruby'}
+words = {1: 'India', 2: 'Britain', 3: 'Python', 4: 'Ruby', 5:'Jaguar', 6:'Nissan', 7:'Mustang'}
 
 # Variable which stores a different word from dictionary each run
 word = words[random.randint(1, len(words))]
