49b479de8a1da526a8a72ab6cbda4e820048b86b
Rishi <rcp0696@gmail.com>
2023-10-22 20:27:01 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+# Importing libraries
+import random
+from math import *
+
+# Creating dictionary of words
+words = {1: 'India', 2: 'Britain', 3: 'Python', 4: 'Ruby'}
+
+# Variable which stores a different word from dictionary each run
+word = words[random.randint(1, len(words))]
+
+print(word)
