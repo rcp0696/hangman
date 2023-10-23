9cb4c9d086911320e1748fbc06869781968c0136
Rishi <rcp0696@gmail.com>
2023-10-23 11:11:09 +0100

    Update project

--- main.py
+++ main.py
@@ -9,3 +9,22 @@ words = {1: 'India', 2: 'Britain', 3: 'Python', 4: 'Ruby', 5:'Jaguar', 6:'Nissan
 word = words[random.randint(1, len(words))]
 
 print(word)
+lives = 10
+found = False
+
+while not found and lives >0:
+    for char in word:
+        print('_',end='')
+    print()
+    userguess = input("Enter a character: ")
+    # Checks if user input is a letter
+    if userguess.isalpha():
+        if userguess in word:
+            found=True
+        else:
+            lives -= 1
+            print("Wrong guess mate")
+    else:
+        print("Please enter letters only")
+
+    print(f"You have {lives} lives left")
