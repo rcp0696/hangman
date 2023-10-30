f833a6ba7ddb0ac7d3c11897c8c776aeb8826d12
Rishi <rcp0696@gmail.com>
2023-10-30 11:38:05 +0000

    Adding a 'lettersguessed' variables for all the letters a user has guessed, attempting to replace underscores with correctly guessed words.

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 # Importing libraries
 import random
-from math import *
 
 # Creating dictionary of words
-words = {1: 'India', 2: 'Britain', 3: 'Python', 4: 'Ruby', 5:'Jaguar', 6:'Nissan', 7:'Mustang'}
+words = {1: 'India', 2: 'Britain', 3: 'Python', 4: 'Ruby', 5: 'Jaguar', 6: 'Nissan', 7: 'Mustang'}
 
 # Variable which stores a different word from dictionary each run
 word = words[random.randint(1, len(words))]
@@ -11,16 +10,23 @@ word = words[random.randint(1, len(words))]
 print(word)
 lives = 10
 found = False
+lettersguessed = ''
 
-while not found and lives >0:
+while not found and lives > 0:
     for char in word:
-        print('_',end='')
+        print('_', end='')
     print()
     userguess = input("Enter a character: ")
+    lettersguessed += userguess
+    print(lettersguessed)
     # Checks if user input is a letter
     if userguess.isalpha():
         if userguess in word:
-            found=True
+            for char in word:
+                if word in lettersguessed:
+                    print(char,end='')
+                else:
+                    print('_',end='')
         else:
             lives -= 1
             print("Wrong guess mate")
