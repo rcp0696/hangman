b688f5885b11ff4758378448b2430f87049d540b
Rishi <rcp0696@gmail.com>
2023-11-06 10:13:13 +0000

    Created a variable which stores all correct user guesses

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 import random
 
 # Creating dictionary of words
-words = {1: 'India', 2: 'Britain', 3: 'Python', 4: 'Ruby', 5: 'Jaguar', 6: 'Nissan', 7: 'Mustang'}
+words = {1: 'india', 2: 'britain', 3: 'python', 4: 'ruby', 5: 'jaguar', 6: 'nissan', 7: 'mustang'}
 
 # Variable which stores a different word from dictionary each run
 word = words[random.randint(1, len(words))]
@@ -11,26 +11,40 @@ print(word)
 lives = 10
 found = False
 lettersguessed = ''
+var = '_' * len(word)
 
 while not found and lives > 0:
-    for char in word:
-        print('_', end='')
-    print()
     userguess = input("Enter a character: ")
     lettersguessed += userguess
-    print(lettersguessed)
     # Checks if user input is a letter
     if userguess.isalpha():
-        if userguess in word:
-            for char in word:
-                if word in lettersguessed:
-                    print(char,end='')
-                else:
-                    print('_',end='')
-        else:
-            lives -= 1
-            print("Wrong guess mate")
+        if len(userguess) == len(word):
+            if userguess == word:
+                print("Nicely done")
+                found = True
+            else:
+                lives -= 1
+
+        elif len(userguess) == 1:
+            if userguess in word:
+                for char in word:
+                    if char in lettersguessed:
+                        # Index of letter guessed in the final word
+                        ind = word.index(userguess)
+                        var = var[:ind] + userguess + var[ind + 1:]
+                        if word.count(userguess) > 1:
+                            for i in range(1, word.count(userguess)):
+                                ind = word.index(userguess)
+                                var = var[:ind] + userguess + var[ind + 1:]
+            else:
+                lives -= 1
+                print("Wrong guess mate")
+            print(var)
+            if var == word:
+                print("You Won! Well Done.")
+                found = True
+
     else:
         print("Please enter letters only")
 
-    print(f"You have {lives} lives left")
+    print(f" You have {lives} lives left")
